513738bd6189db1e95bb791802bb1ab96ec14ce0
ktovar96 <73556529+ktovar96@users.noreply.github.com>
2021-10-17 20:50:18 -0500

    Add files via upload

---
+++ Inventario-Con-Listas-Enlazadas/inventario.js
@@ -0,0 +1,134 @@
+export default class Inventario{
+    constructor(){
+        this.inicio = null;
+    }
+
+    agregar(nuevo){
+        if (this.inicio == null){
+            this.inicio =  nuevo;
+            return nuevo;
+        } else {
+            let aux= this.inicio;
+            if (this.encontrarProducto(nuevo.getId()) == null){
+                while (aux.siguiente != null){
+                    aux = aux.siguiente;
+                }
+                aux.siguiente = nuevo;
+                return nuevo;
+            }
+            return null
+        }
+        
+    }
+
+    encontrarProducto(id){
+        let aux = this.inicio; 
+        while(aux.siguiente != null){
+            aux = aux.siguiente;
+            if (aux.getId() == id){
+                return aux;
+            } else{
+                return null;
+            }
+        }
+    }
+
+    eliminar(id){
+        let elim = null;
+
+        if(this.inicio.getId() == id){
+            elim=this.inicio;
+            this.inicio = this.inicio.siguiente;
+            elim.siguiente = null;
+            return elim;
+        }
+        let aux= this.inicio;
+        while(aux.siguiente != null && elim == null){
+            if(aux.siguiente.getId() == id){
+                elim = aux.siguiente;
+                aux.siguiente = aux.siguiente.siguiente;
+                elim.siguiente = null;
+                return elim;
+            } else {
+                aux = aux.siguiente;
+            }
+        }
+        return elim;
+    }
+
+    listar2(){
+        let texto = "";
+        if (!this.inicio){
+            return "";
+        }
+        let aux = this.inicio;
+        while(aux!=null){
+            texto += aux.info()
+            aux = aux.siguiente;
+        }
+        return texto;
+    }
+
+    listar(){
+        if (!this.inicio){
+            return "";
+        } else {
+            return this._listarRec(this.inicio);
+        }
+    }
+
+    _listarRec(nodo){
+        if (nodo.siguiente == null){
+            return nodo.info();
+        } else {
+            return nodo.info() + this._listarRec(nodo.siguiente) ;
+        }
+    }
+
+    _listarRecInv(nodo){
+        if (nodo.siguiente == null){
+            return nodo.info();
+        } else {
+            return this._listarRecInv(nodo.siguiente) + nodo.info() ;
+        }
+    }
+
+    listarInv(){
+        if (!this.inicio){
+            return "";
+        } else {
+            return this._listarRecInv(this.inicio);
+        }
+    }
+
+    /*insertar(anterior, nuevo){
+        let aux =  this.inicio;
+        while (aux != null){
+            if (aux.siguiente == anterior){
+                nuevo.siguiente = aux.siguiente.siguiente;
+                anterior.siguiente = nuevo;
+                return nuevo;
+            }
+            aux= aux.siguiente;
+        }
+    }*/
+
+        //                            j
+    //(1-> 2 -> 4 -> 5 -> 6)
+                //2
+
+    insertar(pos, nuevo){
+        let aux =  this.inicio;
+        let i = 0;
+            while (aux != null){
+                aux= aux.siguiente;
+                    i++;
+                    if(i == pos){
+                        nuevo.siguiente = aux.siguiente.siguiente;
+                        aux.siguiente = nuevo;
+                        //nuevo.siguiente = aux.siguiente.siguiente;
+                        return nuevo;
+                    }
+            }
+    }
+}
---
+++ Inventario-Con-Listas-Enlazadas/producto.js
@@ -0,0 +1,33 @@
+export default class Prodcut {
+    constructor(id, name, amount, price){
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.price = price;
+        this.siguiente = null;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getAmount(){
+        return this.amount;
+    }
+
+    getPrice(){
+        return this.price;
+    }
+
+    getValue(){
+        return this.amount * this.price;
+    }
+
+    info(){
+        return `<p>ID: ${this.id} Nombre: ${this.name}</p>`;
+    }
+}
